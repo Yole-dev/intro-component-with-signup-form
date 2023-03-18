9d0c428594f2d3303ca7683c8e8e43df658c892e
yole <igbavagodswill4@gmail.com>
2023-03-18 13:53:31 +0000

    found the bug in the script and solved it

--- script.js
+++ script.js
@@ -11,12 +11,16 @@ const lastNameError = document.querySelector('.lastName');
 const emailAddress = document.querySelector('#emailAddress');
 const emailAddressError = document.querySelector('.emailAddress');
 
-const password = document.querySelector('password');
+const password = document.querySelector('#password');
 const passwordError = document.querySelector('.password');
 
 const inputControl = document.querySelector('.input-control');
 
 const claimBtn = document.querySelector('.claimBtn');
+
+const input = [firstName, lastName, emailAddress, password];
+
+
 
 
 //validation
@@ -38,24 +42,24 @@ const validateInputs = function() {
     const emailValue = emailAddress.value.trim();
     const passwordValue = password.value.trim();
 
-    if(firstNameValue = '') {
+    if(firstNameValue == '') {
         firstName.style.borderColor = 'hsl(0, 100%, 74%)';
         firstNameError.innerHTML = `First Name cannot be empty`;
     }
 
-    if(lastNameValue = '') {
+    if(lastNameValue == '') {
         lastName.style.borderColor = 'hsl(0, 100%, 74%)';
         lastNameError.innerHTML = `Last Name cannot be empty`;
     }
 
-    if(emailValue = '') {
+    if(emailValue == '') {
         emailAddress.style.borderColor = 'hsl(0, 100%, 74%)';
         emailAddressError.innerHTML = `Looks like this is not an email`;
     } else if(!isEmailValid(emailValue)) {
         emailAddressError.innerHTML = `Looks like this is not an email`;
     }
 
-    if(passwordValue = '') {
+    if(passwordValue == '') {
         password.style.borderColor = 'hsl(0, 100%, 74%)';
         passwordError.innerHTML = `Password cannot be empty`;
     } else if(passwordValue.length < 8) {
@@ -63,3 +67,13 @@ const validateInputs = function() {
     }
 };
 
+
+//validation with forEach concept
+// claimBtn.addEventListener('click', function(){
+
+//     input.forEach(function(inputEl) {
+//         if(inputEl[i] === '') {
+//             inputEl[i].style.borderColor = 'hsl(0, 100%, 74%)';
+//         }
+//     })
+// })
